3029630dba1cdcac69cc539adf13764905b1c5c9
mozzam123 <mozzam607@gmail.com>
2024-02-23 17:56:41 +0530

    added styling to list component

--- src/components/ListGroup.tsx
+++ src/components/ListGroup.tsx
@@ -1,6 +1,10 @@
+import { MouseEvent } from "react";
+
 function ListGroup() {
   let items = ["New York", "Mumbai", "Pune", "Hyderabad", "Dubai"];
-  items = [];
+
+  // Event Handler
+  const handleClick = (event: MouseEvent) => console.log(event);
 
   return (
     <>
@@ -8,7 +12,9 @@ function ListGroup() {
       {items.length === 0 && <p>No Item Found</p>}
       <ul className="list-group">
         {items.map((item) => (
-          <li key={item}>{item}</li>
+          <li className="list-group-item" key={item} onClick={handleClick}>
+            {item}
+          </li>
         ))}
       </ul>
     </>

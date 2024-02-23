89ec2f9d0aea7413f13cbd7cca1d1d351262f9ac
mozzam123 <mozzam607@gmail.com>
2024-02-23 12:05:27 +0530

    Added New 'List Group Component' in App.tsx

--- src/Message.tsx
+++ src/Message.tsx
@@ -2,7 +2,11 @@
 
 function Message() {
     // Jsx: javascript XML
-  return <h1>Hello world</h1>;
+    const name = ""
+    if (name)
+      return <h1>Hello {name}</h1>;
+    return <h1>Hello world</h1>;
+
 }
 
 export default Message;

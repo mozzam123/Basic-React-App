99a2088771ee3cac044ce72d72ef76fc74572147
mozzam123 <mozzam607@gmail.com>
2024-02-23 12:58:45 +0530

    Added conditional rendering

--- src/components/ListGroup.tsx
+++ src/components/ListGroup.tsx
@@ -1,15 +1,15 @@
 function ListGroup() {
+  let items = ["New York", "Mumbai", "Pune", "Hyderabad", "Dubai"];
+  items = [];
+
   return (
     <>
       <h1>List </h1>
+      {items.length === 0 && <p>No Item Found</p>}
       <ul className="list-group">
-        <li className="list-group-item active" aria-current="true">
-          An active item
-        </li>
-        <li className="list-group-item">A second item</li>
-        <li className="list-group-item">A third item</li>
-        <li className="list-group-item">A fourth item</li>
-        <li className="list-group-item">And a fifth one</li>
+        {items.map((item) => (
+          <li key={item}>{item}</li>
+        ))}
       </ul>
     </>
   );
